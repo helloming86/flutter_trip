5623029fa905852f4755e9bec2ad8390374db570
Nicky <helloming86@gmail.com>
2020-05-01 19:57:28 +0800

    [feat] home banner

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,11 +8,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imagesUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("首页"),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                itemCount: _imagesUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imagesUrls[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
