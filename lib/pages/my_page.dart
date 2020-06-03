c21fdb898dd685b069f28ecfc59fcc224bcb09c4
Nicky <helloming86@gmail.com>
2020-06-03 13:38:50 +0800

    [feat]H5混编实现我的页面

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:trip/widget/webview.dart';
 
 class MyPage extends StatefulWidget {
   @override
@@ -10,8 +11,11 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("个人中心"),
+      body: Webview(
+        url: 'https://m.ctrip.com/webapp/myctrip/',
+        hideAppBar: true,
+        backForbid: true,
+        statusBarColor: '4c5bca',
       ),
     );
   }
